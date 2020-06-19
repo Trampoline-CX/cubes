aa1fe0f58d10f721b9fbadef8745d560004f4711
Alexandre Boucher <alexandre@trampoline.cx>
2020-06-19 12:53:55 -0400

    Add Advanced Dialog example.

--- src/components/overlays/Dialog/Dialog.stories.tsx
+++ src/components/overlays/Dialog/Dialog.stories.tsx
@@ -3,7 +3,9 @@ import { fileAbsolute } from 'paths.macro'
 import { getStoryTitle } from '../../../storybook/get-story-title'
 import { Box } from '../../structure'
 import { Button } from '../../actions'
-import { BodyText } from '../../text'
+import { BodyText, DisplayText, TextContainer, Caption } from '../../text'
+import { LOREM_IPSUM } from '../../../storybook/utils/constants'
+import { Icon } from '../../icons'
 import { Dialog } from './Dialog'
 
 export default {
@@ -30,6 +32,40 @@ export const Basic: React.FC = () => {
         ]}
       >
         <BodyText>Text inside the Dialog!</BodyText>
+      </Dialog>
+    </Box>
+  )
+}
+
+export const Advanced: React.FC = () => {
+  const [visible, setVisible] = useState(false)
+
+  return (
+    <Box padding="medium" align="start">
+      <Button onClick={() => setVisible(true)}>Show me how!</Button>
+      <Dialog
+        title="Review"
+        open={visible}
+        onClose={() => setVisible(false)}
+        primaryAction={{ label: 'Accept', action: () => setVisible(false) }}
+        secondaryActions={[{ label: 'Decline', action: () => setVisible(false) }]}
+      >
+        <Dialog.Section>
+          <TextContainer>
+            <DisplayText>Terms and Conditions</DisplayText>
+            <BodyText>{LOREM_IPSUM}</BodyText>
+            <BodyText>{LOREM_IPSUM}</BodyText>
+          </TextContainer>
+        </Dialog.Section>
+        <Dialog.Section>
+          <Box horizontal align="center" space="small">
+            <Icon name="feedback" />
+            <BodyText>
+              If feeling confident, you can put anything in the Modal content and even have multiple
+              sections...
+            </BodyText>
+          </Box>
+        </Dialog.Section>
       </Dialog>
     </Box>
   )
