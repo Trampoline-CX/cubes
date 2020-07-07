d07a0445c2fb46b5c0a34b0f141e86c8abd81232
Alexandre Boucher <alexandre@trampoline.cx>
2020-07-07 09:02:32 -0400

    Remove transformSource as function text is now removed by Storybook itself.

--- storybook/web/preview.js
+++ storybook/web/preview.js
@@ -2,16 +2,7 @@ import { addParameters } from '@storybook/react'
 import { Background } from '../../src/storybook/decorators/Background'
 import { WithAppProvider } from '../../src/storybook/decorators/WithAppProvider'
 
-// TODO: Replace with this once merged: https://github.com/storybookjs/storybook/issues/10758
-const SOURCE_REGEX = /^(\(\)|[A-Z]+) => ((.|\n)+)$/im // Remove leading props and parentheses
-
 addParameters({
-  docs: {
-    transformSource: src => {
-      const match = SOURCE_REGEX.exec(src)
-      return match ? match[2] : src
-    },
-  },
   options: {
     // Sort stories based on name
     storySort: (a, b) =>
