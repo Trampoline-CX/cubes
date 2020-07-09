75d414e598f1c0ed339afcb00f1648853166f5a1
Alexandre Boucher <alexandre@trampoline.cx>
2020-07-09 17:38:16 -0400

    Dual rendering of Storybook Stories to have light and dark themes one beside another. Chromatic stories are now rendered as stacked.

--- storybook/web/preview.js
+++ storybook/web/preview.js
@@ -1,4 +1,5 @@
 import { addParameters } from '@storybook/react'
+import isChromatic from 'chromatic/isChromatic'
 import { Background } from '../../src/storybook/decorators/Background'
 import { WithAppProvider } from '../../src/storybook/decorators/WithAppProvider'
 
@@ -16,12 +17,14 @@ export const globalTypes = {
   theme: {
     name: 'Theme',
     description: 'Global theme for components',
-    defaultValue: 'light',
+    defaultValue: isChromatic() ? 'stacked' : 'light',
     toolbar: {
       icon: 'circlehollow',
       items: [
         { value: 'light', icon: 'circlehollow', title: 'Light' },
         { value: 'dark', icon: 'circle', title: 'Dark' },
+        { value: 'side-by-side', icon: 'sidebar', title: 'side by side' },
+        { value: 'stacked', icon: 'bottombar', title: 'stacked' },
       ],
     },
   },
