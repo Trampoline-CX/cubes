d75b79587383a174fd0dbb3abee6862ccf71885f
Alexandre Boucher <alexandre@trampoline.cx>
2020-07-15 16:25:10 -0400

    Add error icon to InlineError

--- src/components/forms/InlineError/InlineError.stories.tsx
+++ src/components/forms/InlineError/InlineError.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { fileAbsolute } from 'paths.macro'
 import { getStoryTitle } from '../../../storybook/get-story-title'
+import { LOREM_IPSUM } from '../../../storybook/utils/constants'
 import { InlineError } from './InlineError'
 
 export default {
@@ -9,3 +10,5 @@ export default {
 }
 
 export const Default: React.FC = () => <InlineError message="An error occured while doing stuff." />
+
+export const WithLongText: React.FC = () => <InlineError message={LOREM_IPSUM} />
