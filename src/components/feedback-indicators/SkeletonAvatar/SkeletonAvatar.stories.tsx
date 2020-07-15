1f898794397fa3f259e6fe5e0eba95311d5720dc
Alexandre Boucher <alexandre@trampoline.cx>
2020-07-15 12:08:43 -0400

    Add size prop on SkeletonAvatar and SkeletonIcon

--- src/components/feedback-indicators/SkeletonAvatar/SkeletonAvatar.stories.tsx
+++ src/components/feedback-indicators/SkeletonAvatar/SkeletonAvatar.stories.tsx
@@ -2,15 +2,22 @@ import React from 'react'
 import { fileAbsolute } from 'paths.macro'
 import { SkeletonLoading } from '../SkeletonLoading/SkeletonLoading'
 import { getStoryTitle } from '../../../storybook/get-story-title'
-import { SkeletonAvatar } from './SkeletonAvatar'
+import { StoryFn } from '../../../storybook/utils/storybook-types'
+import { SkeletonAvatar, SkeletonAvatarProps } from './SkeletonAvatar'
 
 export default {
   title: getStoryTitle(fileAbsolute),
   component: SkeletonAvatar,
 }
 
-export const Default: React.FC = () => (
+export const Default: StoryFn<SkeletonAvatarProps> = props => (
   <SkeletonLoading loading>
-    <SkeletonAvatar />
+    <SkeletonAvatar {...props} />
+  </SkeletonLoading>
+)
+
+export const Small: React.FC = () => (
+  <SkeletonLoading loading>
+    <SkeletonAvatar size="small" />
   </SkeletonLoading>
 )
--- src/components/feedback-indicators/SkeletonIcon/SkeletonIcon.stories.tsx
+++ src/components/feedback-indicators/SkeletonIcon/SkeletonIcon.stories.tsx
@@ -2,15 +2,34 @@ import React from 'react'
 import { fileAbsolute } from 'paths.macro'
 import { SkeletonLoading } from '../SkeletonLoading/SkeletonLoading'
 import { getStoryTitle } from '../../../storybook/get-story-title'
-import { SkeletonIcon } from './SkeletonIcon'
+import { StoryFn } from '../../../storybook/utils/storybook-types'
+import { SkeletonIcon, SkeletonIconProps } from './SkeletonIcon'
 
 export default {
   title: getStoryTitle(fileAbsolute),
   component: SkeletonIcon,
 }
 
-export const Default: React.FC = () => (
+export const Default: StoryFn<SkeletonIconProps> = props => (
   <SkeletonLoading loading>
-    <SkeletonIcon />
+    <SkeletonIcon {...props} />
+  </SkeletonLoading>
+)
+
+export const Small: React.FC = () => (
+  <SkeletonLoading loading>
+    <SkeletonIcon size="small" />
+  </SkeletonLoading>
+)
+
+export const Large: React.FC = () => (
+  <SkeletonLoading loading>
+    <SkeletonIcon size="large" />
+  </SkeletonLoading>
+)
+
+export const XLarge: React.FC = () => (
+  <SkeletonLoading loading>
+    <SkeletonIcon size="xLarge" />
   </SkeletonLoading>
 )
