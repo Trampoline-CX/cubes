53c024331cd6e9f72283bb51e99372feeff1c14b
Hans Gazaille <hans@trampoline.cx>
2020-07-16 21:45:14 -0400

    Group more size stories together

--- src/components/feedback-indicators/SkeletonAvatar/SkeletonAvatar.stories.tsx
+++ src/components/feedback-indicators/SkeletonAvatar/SkeletonAvatar.stories.tsx
@@ -3,6 +3,9 @@ import { fileAbsolute } from 'paths.macro'
 import { SkeletonLoading } from '../SkeletonLoading/SkeletonLoading'
 import { getStoryTitle } from '../../../storybook/get-story-title'
 import { StoryFn } from '../../../storybook/utils/storybook-types'
+import { Box } from '../../structure/Box/Box'
+import { Heading } from '../../text'
+import { useTheme } from '../../../theme'
 import { SkeletonAvatar, SkeletonAvatarProps } from './SkeletonAvatar'
 
 export default {
@@ -16,8 +19,19 @@ export const Default: StoryFn<SkeletonAvatarProps> = props => (
   </SkeletonLoading>
 )
 
-export const Small: React.FC = () => (
-  <SkeletonLoading loading>
-    <SkeletonAvatar size="small" />
-  </SkeletonLoading>
-)
+export const Size: React.FC = () => {
+  const { size } = useTheme()
+
+  return (
+    <Box horizontal space="xLarge">
+      <Box>
+        <Heading>Small ({size.avatar.small}dp)</Heading>
+        <SkeletonAvatar size="small" />
+      </Box>
+      <Box>
+        <Heading>Default ({size.avatar.default}dp)</Heading>
+        <SkeletonAvatar size="default" />
+      </Box>
+    </Box>
+  )
+}
