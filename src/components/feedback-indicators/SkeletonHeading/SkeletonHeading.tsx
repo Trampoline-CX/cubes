52f28aa9ae9946e713c3c379599b7afa8ba9f77c
Alexandre Boucher <alexandre@trampoline.cx>
2020-09-11 15:26:52 -0400

    Fix lint issue

--- src/components/feedback-indicators/SkeletonHeading/SkeletonHeading.tsx
+++ src/components/feedback-indicators/SkeletonHeading/SkeletonHeading.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { View, StyleSheet } from 'react-native'
-import { Box } from '../../structure/Box/Box'
 import { shameStyles } from '../../../theme/shame-styles'
 import { SkeletonBodyText } from '../SkeletonBodyText/SkeletonBodyText'
 
